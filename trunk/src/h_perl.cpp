e10d60ebe3a770cd5eb6d4fa9de355780bd8878e
tanktalus <tanktalus@140b2063-a995-4ee3-92f6-8f1e8d500c33>
2001-03-15 02:43:51 +0000

    Missed one case on perl highlighting

--- trunk/src/h_perl.cpp
+++ trunk/src/h_perl.cpp
@@ -327,7 +327,9 @@ int Hilit_PERL(EBuffer *BF, int /*LN*/, PCell B, int Pos, int Width, ELine *Line
                     goto hilit;
                 } else if (X_NOT(State) &&
                            *p == '-' &&
-                           len >= 2) {
+                           len >= 2 &&
+                           isalpha(p[1])
+                          ) {
                     Color = Colors[CLR_Normal]; // default.
                     if (strchr("wrxoRWXOezsfdlpSbctugkTB", p[1]) != NULL) {
                         Color = Colors[CLR_Punctuation]; // new default.
