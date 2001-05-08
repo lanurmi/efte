6f1d89e09c0ff787808ab4ceae634cb015d303f0
tanktalus <tanktalus@140b2063-a995-4ee3-92f6-8f1e8d500c33>
2001-05-08 00:03:42 +0000

    update cfte to proper version

--- src/ftever.h
+++ src/ftever.h
@@ -4,8 +4,8 @@
 #define PROG_FTE   "fte"
 #define PROG_CFTE  "cfte"
 #define PROGRAM    PROG_FTE
-#define VERSION    "0.49.10"
+#define VERSION    "0.49.13"
 #define VERNUM     0x00490400UL
-#define COPYRIGHT  "Copyright (c) 1994-1998 Marko Macek"
-
+#define COPYRIGHT  "Copyright (c) 1994-1998 Marko Macek\n" \
+                   "Copyright (c) 2000-2001 Others"
 #endif
