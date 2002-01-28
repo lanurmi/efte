24c1005824bc43a7ccb3d0d0ee456a4c7302249c
tanktalus <tanktalus@140b2063-a995-4ee3-92f6-8f1e8d500c33>
2002-01-28 23:47:14 +0000

    New version for binary config files

--- src/ftever.h
+++ src/ftever.h
@@ -5,7 +5,7 @@
 #define PROG_CFTE  "cfte"
 #define PROGRAM    PROG_FTE
 #define VERSION    "0.49.13"
-#define VERNUM     0x00490400UL
+#define VERNUM     0x00490401UL
 #define COPYRIGHT  "Copyright (c) 1994-1998 Marko Macek\n" \
                    "Copyright (c) 2000-2001 Others"
 #endif
