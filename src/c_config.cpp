da795bd7c0e0c3fca4ce21619da59bbd4033e559
chaac <chaac@140b2063-a995-4ee3-92f6-8f1e8d500c33>
2003-06-05 19:10:00 +0000

    Switched from safe_strncpy to BSD style strlcpy.

--- src/c_config.cpp
+++ src/c_config.cpp
@@ -294,15 +294,15 @@ static int SetGlobalString(long what, const char *string) {
     LOG << "What: " << what << " String: " << string << ENDLINE;
 
     switch (what) {
-    case FLAG_DefaultModeName: safe_strncpy(DefaultModeName, string, sizeof(DefaultModeName)); break;
+    case FLAG_DefaultModeName: strlcpy(DefaultModeName, string, sizeof(DefaultModeName)); break;
     case FLAG_CompletionFilter: if ((CompletionFilter = RxCompile(string)) == NULL) return -1; break;
-    case FLAG_PrintDevice: safe_strncpy(PrintDevice, string, sizeof(PrintDevice)); break;
-    case FLAG_CompileCommand: safe_strncpy(CompileCommand, string, sizeof(CompileCommand)); break;
-    case FLAG_WindowFont: safe_strncpy(WindowFont, string, sizeof(WindowFont)); break;
-    case FLAG_HelpCommand: safe_strncpy(HelpCommand, string, sizeof(HelpCommand)); break;
+    case FLAG_PrintDevice: strlcpy(PrintDevice, string, sizeof(PrintDevice)); break;
+    case FLAG_CompileCommand: strlcpy(CompileCommand, string, sizeof(CompileCommand)); break;
+    case FLAG_WindowFont: strlcpy(WindowFont, string, sizeof(WindowFont)); break;
+    case FLAG_HelpCommand: strlcpy(HelpCommand, string, sizeof(HelpCommand)); break;
     case FLAG_GUICharacters: AppendGUICharacters (string); break;
-    case FLAG_CvsCommand: safe_strncpy(CvsCommand, string, sizeof(CvsCommand)); break;
-    case FLAG_CvsLogMode: safe_strncpy(CvsLogMode, string, sizeof(CvsLogMode)); break;
+    case FLAG_CvsCommand: strlcpy(CvsCommand, string, sizeof(CvsCommand)); break;
+    case FLAG_CvsLogMode: strlcpy(CvsLogMode, string, sizeof(CvsLogMode)); break;
     default:
         //printf("Unknown global string: %ld\n", what);
         ENDFUNCRC(-1);
