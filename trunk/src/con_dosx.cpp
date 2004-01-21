394da9e74e65319f059ba43831da6452a6502aab
chaac <chaac@140b2063-a995-4ee3-92f6-8f1e8d500c33>
2004-01-21 22:27:13 +0000

    Added include for s_string.h.

--- trunk/src/con_dosx.cpp
+++ trunk/src/con_dosx.cpp
@@ -23,10 +23,11 @@
 #include "console.h"
 #include "c_config.h"
 #include "gui.h"
+#include "s_string.h"
 
 #include <stdlib.h>
 #include <process.h>
-#include	"port.h"                // DOS portability calls,
+#include "port.h"                // DOS portability calls,
 
 #define MAX_PIPES 4
 #define PIPE_BUFLEN 4096
