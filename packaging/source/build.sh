b2384aaa77a13d859b9d4d7decd3bad64a97ed8c
Jeremy Cowgar <jcowgar@users.noreply.github.com>
2008-02-14 20:35:12 +0000

    * Added source packaging tool * Set version number to 0.95

---
+++ packaging/source/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+NAME=efte
+
+VERSIONFILE="../../src/ftever.h"
+DEFAULTVERSION="$(sed -n 's/^\#define VERSION *\"//p' $VERSIONFILE |sed 's/\".*$//')"
+VERSION=${1:-$DEFAULTVERSION}        # use DEFAULT version if not specified on command line
+[[ -z $VERSION ]] && exit 1          # no version, so exit if none specified and none found
+
+BASEPACKNAME=${NAME}-${VERSION}
+
+if [[ -d ${BASEPACKNAME} ]] ; then
+  echo "Destination already exists. Previous packages with the same version number"
+  echo "will be removed and existing destination will be renamed."
+  echo "ctrl-C aborts, enter continues..."
+  read
+  mv "$BASEPACKNAME"  "${BASEPACKNAME}.$(date +%s)"
+  rm -f "$BASEPACKNAME".tar.gz "$BASEPACKNAME".tar.bz2 "$BASEPACKNAME".zip
+fi
+
+svn co http://efte.svn.sourceforge.net/svnroot/efte/tags/${VERSION} ${BASEPACKNAME} &&
+find "$BASEPACKNAME" -type d -name .svn | while read DIR ; do rm -r "$DIR" ; done
+
+tar czf ${BASEPACKNAME}.tar.gz ${BASEPACKNAME}
+tar cjf ${BASEPACKNAME}.tar.bz2 ${BASEPACKNAME}
+zip -r ${BASEPACKNAME}.zip ${BASEPACKNAME}
--- src/ftever.h
+++ src/ftever.h
@@ -16,9 +16,8 @@
 #define PROG_FTE      "efte"
 #define PROG_CFTE     "cefte"
 #define PROGRAM       PROG_FTE
-#define EXTRA_VERSION ""
-#define VERSION       "1.0" EXTRA_VERSION
-#define VERNUM        MAKE_VERSION(0x01, 0x00, 0x00)
+#define VERSION       "0.95"
+#define VERNUM        MAKE_VERSION(0x00, 0x5F, 0x00)
 #define COPYRIGHT     "Copyright (c) 2008 eFTE Group\n" \
    "Copyright (c) 2000-2006 Others\n" \
    "Copyright (c) 1994-1998 Marko Macek"
