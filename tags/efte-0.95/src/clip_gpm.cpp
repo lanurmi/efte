51a01113a8b716c1a15e109b1a5aea77a65b2a44
jeremy_c <jeremy_c@140b2063-a995-4ee3-92f6-8f1e8d500c33>
2008-02-15 01:32:16 +0000

    Tagging for 0.95 release

---
+++ tags/efte-0.95/src/clip_gpm.cpp
@@ -0,0 +1,19 @@
+/*    clip_gpm.cpp
+ *
+ *    Copyright (c) 2008, eFTE SF Group (see AUTHORS file)
+ *    Copyright (c) 1994-1996, Marko Macek
+ *
+ *    You may distribute under the terms of either the GNU General Public
+ *    License or the Artistic License, as specified in the README file.
+ *
+ */
+
+#include "fte.h"
+
+int GetPMClip(int clipboard) {
+    return 0;
+}
+
+int PutPMClip(int clipboard) {
+    return 0;
+}
