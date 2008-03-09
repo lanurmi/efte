a928f24e2fc7f36350d5bcf4b7412de386f1fedd
Jeremy Cowgar <jcowgar@users.noreply.github.com>
2008-03-09 21:56:12 +0000

    * Added Makefile.mingw for windows compiling * Fixed a few recent additions so that they compile under windows. * Made the mkdir() function ifdef/else properly for mingw

--- src/s_util.cpp
+++ src/s_util.cpp
@@ -247,19 +247,23 @@ int CompletePath(const char *Base, char *Match, int Count) {
 }
 
 
-#if ndef HAVE_GETTIMEOFDAY
-
-#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS) || defined
-(__BORLANDC__)
+#ifndef HAVE_GETTIMEOFDAY
+#if defined(_MSC_VER) || defined(_MSC_EXTENSIONS) || defined(__BORLANDC__)
   #define DELTA_EPOCH_IN_MICROSECS  11644473600000000Ui64
 #else
   #define DELTA_EPOCH_IN_MICROSECS  11644473600000000ULL
 #endif
+
+#ifdef NT
+#include <windows.h>
+#endif
+
 struct timezone
 {
   int  tz_minuteswest; /* minutes W of Greenwich */
   int  tz_dsttime;     /* type of dst correction */
 };
+
 int gettimeofday(struct timeval *tv, struct timezone *tz)
 {
   FILETIME ft;
