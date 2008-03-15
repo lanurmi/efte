b984e70b4318b2a37e4ea43f506e5ef0d8e36587
Forthfreak <Forthfreak@140b2063-a995-4ee3-92f6-8f1e8d500c33>
2008-03-15 00:38:30 +0000

    fancy firescript shell autoformatter, experimental. try, after loading tools.f: : foo         <keypad enter> begin         <keypad enter> 0             <keypad enter> while         <keypad enter> "foo" type$   <keypad enter> repeat        <keypad enter> ;             <keypad enter> input will be properly indented as you enter it.
    M    config/tools.f
    M    config/interpreter.fte

--- trunk/config/tools.f
+++ trunk/config/tools.f
@@ -26,6 +26,16 @@
 : pairedwith       ( x1 x2 -- )    = unless "unmatching conditionals" error ;
 : pairedwitheither ( x1 x2 x3 -- ) >r over = swap r> = or true pairedwith ;
 
+: +indent  2 shellindent +! ;
+
+: -indent -2 shellindent +!    \ a bit unwieldy without loops yet.
+    cursorcol cursorhome
+    read bl = ?? killchar
+    read bl = ?? killchar
+    movetocolumn    
+;
+
+
 \ --- flow control tool kit ---
 : mark         ( -- a )        here  ;
 : offset       ( a1 a2 -- n )  1 + -  ;
@@ -39,22 +49,24 @@
 : ?backwards   ( a -- )        branch0, <resolve  ;
 
 \ --- flow control statements ---
-: if     ?comp                                       ?forwards              me ; immediate
-: else   ?comp      ['] if          pairedwith        forwards swap resolve me ; immediate
-: endif  ?comp      ['] if ['] else pairedwitheither                resolve    ; immediate
-: begin  ?comp                                                       mark   me ; immediate
-: while  ?comp      ['] begin       pairedwith       ?forwards              me ; immediate
-: repeat ?comp      ['] while       pairedwith  swap  backwards     resolve    ; immediate
-: until  ?comp      ['] begin       pairedwith       ?backwards                ; immediate
-: again  ?comp      ['] begin       pairedwith        backwards                ; immediate
+: if     ?comp  +indent                                          ?forwards              me ; immediate
+: else   ?comp  -indent +indent ['] if          pairedwith        forwards swap resolve me ; immediate
+: endif  ?comp  -indent         ['] if ['] else pairedwitheither                resolve    ; immediate
+: begin  ?comp  +indent                                                          mark   me ; immediate
+: while  ?comp  -indent +indent ['] begin       pairedwith       ?forwards              me ; immediate
+: repeat ?comp  -indent         ['] while       pairedwith  swap  backwards     resolve    ; immediate
+: until  ?comp  -indent         ['] begin       pairedwith       ?backwards                ; immediate
+: again  ?comp  -indent         ['] begin       pairedwith        backwards                ; immediate
 
-: do     
+: do
     ?comp 
+    +indent
     ['] dodo , 
     mark  0, 
     me ;  immediate
         
 : loop  ?comp 
+    -indent
     ['] do pairedwith 
     ['] doloop , 
     dup 2 + <resolve
