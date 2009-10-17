58df02dbfc7e11ee11c7dce6976465ad345826e4
Lauri Nurmi <lanurmi@users.noreply.github.com>
2009-10-17 15:17:03 +0000

    Added normal copyright/license headers to Obj-C(++) files.

--- src/FTETextAreaView.h
+++ src/FTETextAreaView.h
@@ -1,4 +1,11 @@
-/* FTETextAreaView */
+/*    FTETextAreaView.m
+ *
+ *    Copyright (c) 2008, eFTE SF Group (see AUTHORS file)
+ *
+ *    You may distribute under the terms of either the GNU General Public
+ *    License or the Artistic License, as specified in the README file.
+ *
+ */
 
 #import <Cocoa/Cocoa.h>
 
