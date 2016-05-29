63ae78f1ab630ef7b35317b19a8dc53c24630c7e
Lauri Nurmi <lanurmi@iki.fi>
2016-05-29 20:37:20 +0300

    Update year to 2016.

--- src/ftever.h
+++ src/ftever.h
@@ -19,7 +19,7 @@
 #define EXTRA_VERSION ""
 #define VERSION       "1.1" EXTRA_VERSION
 #define VERNUM        MAKE_VERSION(0x00, 0x62, 0x00)
-#define COPYRIGHT     "Copyright (c) 2008-2009 eFTE Group\n" \
+#define COPYRIGHT     "Copyright (c) 2008-2016 eFTE Group\n" \
    "Copyright (c) 2000-2006 Others\n" \
    "Copyright (c) 1994-1998 Marko Macek"
 
